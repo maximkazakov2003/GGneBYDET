a52ad6159c39e7c860326ec4362f305c23459fad
maximkazakov2003 <svetlanakazak2014@gmail.com>
2019-11-03 14:09:34 +0300

    comment

--- src/What.java
+++ src/What.java
@@ -4,13 +4,15 @@ public class What {
         Cat jopa = new Cat();
         Popug myay = new Popug();
         Enchik Wova = new Enchik();
+
         gaf.all_data(" Король Ночи", "6", "верховно-черный","СМЭЭЭЭЭЭЭрть"       );
         System.out.println(gaf.getall_data());
         jopa.all_data(" СВЯТОЙ МЯУКАЛО", "8", "Серо-буро-малиновый","мяу" );
         System.out.println(jopa.getall_data());
         myay.all_data(" Летучая мышь", "1", "темненький","Попка дурак"       );
         System.out.println(myay.getall_data());
-        Wova.all_data(" Воришка Вова", "10", "темно-белыйв полску","Убью"       );
+        Wova.all_data(" Воришка Вова", "10", "темно-белый в полску","хыхыххыхыхыы"       );
         System.out.println(Wova.getall_data());
+
     }
 }
